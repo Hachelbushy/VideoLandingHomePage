690345a5ae4131bd5eb663c9e38ba6a32f93b81a
Matthew <mateuszzakrzewskipl@gmail.com>
2020-08-22 18:20:42 +0200

    Multilanguage site
    Dodanie przycisków(PL, EN)
    Dodanie skryptu odpowiedzialnego za wyświetlanie odpowiedniego teksu w zależności od obranego języka.

---
+++ distribution/js/main.js
@@ -0,0 +1,40 @@
+const langEl = document.querySelector('.langWrap');
+const link = document.querySelectorAll('a');
+const headingEl = document.querySelector('.heading');
+const btnEl = document.querySelector('.btn')
+const aboutEl = document.querySelector('.aboutHeading');
+const socialEl =  document.querySelector('.socialMedia');
+
+
+link.forEach(el =>{
+    el.addEventListener('click',() => {
+        langEl.querySelector('.active').classList.remove('active');
+        el.classList.add('active');
+
+
+        const attr = el.getAttribute('lang');
+        // const attr2 = el.getAttribute('btn');
+        // const attr3 = [attr,attr2];
+
+        headingEl.textContent = headingText[attr].heading;
+        btnEl.textContent = headingText[attr].btn
+        aboutEl.textContent = headingText[attr].aboutHeading;
+        socialEl.textContent = headingText[attr].socialMedia;
+
+    });
+});
+
+var headingText = {
+    'english':{
+        'socialMedia':'Follow Me On Social Media!',
+        'aboutHeading': 'Full screen video landing page',
+        'btn': 'Read More',
+        'heading': 'Shoot for the moon. Even if you miss, you will land among the stars.'
+    },
+    'polish':{
+        'socialMedia': 'Śledź Mnie W Mediach Społecznościowych!',
+        'aboutHeading': 'Strona główna z pełnoekranowym plikiem video',
+        'btn': 'Czytaj Więcej',
+        'heading': 'Celuj w księżyc, bo nawet jeśli nie trafisz, będziesz między gwiazdami.'
+    }
+}
